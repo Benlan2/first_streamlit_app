531283634c6e78e1943ad36a1f6811737042e402
Benlan2 <123488213+Benlan2@users.noreply.github.com>
2023-01-26 01:40:53 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ streamlit.dataframe(fruits_to_show)
 
 #New Section to display fruityvice api response
 def get_fruityvice_data(this_fruit_choice):
-  fruityvice_response = requests.get("https//fruityvice.com/api/fruit/" + this_fruit_choice)
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
 
@@ -36,7 +36,7 @@ try:
     streamlit.error("Please select a fruit to get infomration.")
   else:
     back_from_function = get_fruityvice_data(fruit_choice)
-    stremlit.dataframe(back_from_function)
+    streamlit.dataframe(back_from_function)
 except URLError as e:
     streamlite.error()
 #streamlit.write('The user entered ', fruit_choice)
